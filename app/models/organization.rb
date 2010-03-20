e7623196ae5f6c1c88c8f8700a0ddb90c0820c10
Dan Thomas <dan.thomas@ipublic.org>
2010-03-20 18:05:11 -0400

    Extensive changes to improve CouchDB interface, including validation, callbacks, ID generation, property typing

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,30 +1,52 @@
 class Organization < CouchRestRails::Document
-  
-  use_database :site
-  unique_id :url
-  
-  property :name
-  property :url
-  property :point_of_contact, :cast_as => 'Contact'
-  property :description
-#  property :org_key
 
-#  set_callback :save, :before, :generate_org_key
+# see http://github.com/hpoydar/couchrest-rails
+  require 'contact'
+  require 'address'
+  include CouchRest::Validation
+  
+  use_database :schema
+  unique_id :identifier
+  
+#  attr_accessor :url
+  
+  property :name, :length => 1...50
+  property :abbreviation
+  property :identifier, :length => 1...100
+  property :point_of_contact, :cast_as => 'Contact', :default => []
+  property :address, :cast_as => 'Address', :default => []
+  property :website_url
+  property :description
+  timestamps!
 
   ## Validations
   validates_presence_of :name
-
+  set_callback :save, :before, :generate_identifier
+  set_callback :save, :before, :generate_contact_fullname
+  
   ## CouchDB Views
   # query with Organization.by_name
   view_by :name
 
-  # build key from organization name + contact's email address (if provided)
-  # def generate_org_key
-  #   org_key << "#{name}-" if name
-  #   org_key << "-#{point_of_contact.email}" if point_of_contact.email
-  # 
-  #   self['org_key'] = org_key
-  # end
+private
+  ## This constant assignment will throw error when DB is first initialized 
+  ## (until model views are loaded to CouchDB)
+  NAMES_IDS = self.all.map do |m|
+    [m.name, m.identifier]
+  end  
 
+  def generate_identifier
+    unless abbreviation.blank? 
+      self['identifier'] = self['couchrest-type'].to_s.pluralize.downcase + '_'+ abbreviation.rstrip.downcase.gsub(/[^a-z0-9]/,'_').squeeze('_').gsub(/^\-|\-$/,'') if new?
+    else
+      self['identifier'] = self['couchrest-type'].to_s.pluralize.downcase + '_'+ name.rstrip.downcase.gsub(/[^a-z0-9]/,'_').squeeze('_').gsub(/^\-|\-$/,'') if new?
+    end
+#    self['identifier'] = name.downcase.gsub(/[^a-z0-9]/,'_').squeeze('_').gsub(/^\-|\-$/,'') if new?
+  end
+  
+  def generate_contact_fullname
+    self.point_of_contact['full_name'] = self.point_of_contact['first_name'] + ' ' + self.point_of_contact['last_name']
+  end
+  
 end
 
