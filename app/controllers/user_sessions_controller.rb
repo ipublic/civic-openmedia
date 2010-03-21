c151bbb06a6a78a8eeb9548052fdc6a45bf34af4
Dan Thomas <dan.thomas@ipublic.org>
2010-03-20 20:19:08 -0400

    Removed flash notice for successful login

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -9,7 +9,6 @@ class UserSessionsController < ApplicationController
   def create
     @user_session = UserSession.new(params[:user_session])
     if @user_session.save
-      flash[:notice] = "Login successful!"
       redirect_back_or_default root_path
     else
       render :action => :new
