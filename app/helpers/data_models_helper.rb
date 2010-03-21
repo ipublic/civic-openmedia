3528090be3a0cd8dfec929fa92d4ee67b8ee9524
Dan Thomas <dan.thomas@ipublic.org>
2010-03-20 20:20:24 -0400

    added organization_lookup method

--- app/helpers/data_models_helper.rb
+++ app/helpers/data_models_helper.rb
@@ -1,5 +1,11 @@
 module DataModelsHelper
-
+  
+  def organization_lookup(key)
+    #Use the NAMES_IDS constant on Organization model to find Organization model given a key
+    org_pair = Organization::NAMES_IDS.rassoc(key)
+    org_name = org_pair.nil? ? "Unknown organization" : org_pair[0]
+  end
+  
   def add_property_definition_link(name) 
     link_to_function name do |page| 
       page.insert_html :bottom, :property_definitions, :partial => 'property_definition', :object => PropertyDefinition.new 
