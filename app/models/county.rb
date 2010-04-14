403751b0ba86da8231e64c123306bd8c006e6d19
Dan Thomas <dan.thomas@ipublic.org>
2010-04-14 11:50:49 -0400

    updated IDs to prefix with classname.  revised seed.rb to load state and county values

--- app/models/county.rb
+++ app/models/county.rb
@@ -4,9 +4,10 @@ class County < CouchRestRails::Document
   
   ## CouchDB database and record key
   use_database :community
-  unique_id :fips_code # coumpound natural key
+  unique_id :identifier
   
   ## Properties
+  property :identifier
   property :name
   property :state_fips_code
   property :county_fips_code
@@ -14,35 +15,34 @@ class County < CouchRestRails::Document
 
   timestamps!
   
-  set_callback :save, :before, :generate_fips_code
-
   ## Validations
-  validates_presence_of :name
-  validates_presence_of :state_fips_code
-  validates_presence_of :county_fips_code
-  validates_numericality_of :county_fips_code
-  validates_numericality_of :state_fips_code
-  validates_with_method :state_fips_code, :method => :check_fips_uniqueness
+  validates_presence_of :name, :state_fips_code, :county_fips_code
+#  validates_with_method :state_fips_code, :method => :check_fips_uniqueness
+  
+  before_save :generate_identifier
   
   ## CouchDB Views
-  # query with County.by_name
+  # query with:
+  #   County.by_name - all counties sorted by name
+  #   County.by_state_fips_code(:key => '02') - all counties that match FIPS code for Alaska
+  #   County.get("county_02_270") - specific county by ID
+  
   view_by :name
-
-
-  def generate_fips_code
-    fips_str = self.class.to_s + '-'
-    fips_str << "#{state_fips_code}, " if state_fips_code
-    fips_str << "#{county_fips_code}, " if county_fips_code
-
-    self['fips_code'] = fips_str
-  end
+  view_by :state_fips_code
   
-  def check_fips_uniqueness
-    if self.new_document? && County.fips_code(:key => self.fips_code).length > 0
-      return [false, "A county has already been created with this FIPS code"]
-    end
-    return true
-  end
+  # def check_fips_uniqueness
+  #   if self.new_document? && County.fips_code(:key => self.fips_code).length > 0
+  #     return [false, "A county has already been created with this FIPS code"]
+  #   end
+  #   return true
+  # end
   
+private
+  def generate_identifier
+    fips_str = "#{state_fips_code}_#{county_fips_code}"
+
+    #Pattern for Unique ID: class_key
+    self['identifier'] = self.class.to_s.downcase + '_' + fips_str if new?
+  end
 
 end
--- app/models/state.rb
+++ app/models/state.rb
@@ -4,9 +4,10 @@ class State < CouchRestRails::Document
   
   ## CouchDB database and record key
   use_database :community
-  unique_id :abbreviation #natural key
+  unique_id :identifier 
   
   ## Properties
+  property :identifier
   property :name, :length => 1...20
   property :abbreviation, :length => 1...2
   property :state_fips_code #, :read_only => true
@@ -15,11 +16,12 @@ class State < CouchRestRails::Document
   timestamps!
 
   ## Validations
-  validates_presence_of :name
-  validates_numericality_of :state_fips_code
-  validates_with_method :abbreviation, :method => :check_abbreviation_uniqueness
+  validates_presence_of :name, :abbreviation, :state_fips_code
+#  validates_with_method :abbreviation, :method => :check_abbreviation_uniqueness
 
   before_save :set_text_case
+  before_save :generate_identifier
+#  set_callback :save, :before, :generate_identifier
   
   ## CouchDB Views
   # query with State.by_name or State.by_fips_code
@@ -29,17 +31,16 @@ class State < CouchRestRails::Document
 
   ## This constant assignment will throw error when DB is first initialized 
   ## (until model views are loaded to CouchDB)
-  NAMES_IDS = self.all.map do |m|
-    [m.name, m.abbreviation]
-  end  
+  # NAMES_IDS = self.all.map do |m|
+  #   [m.name, m.abbreviation]
+  # end  
   
-  def check_abbreviation_uniqueness
-    if self.new_document? && State.get(self.abbreviation.upcase).nil?
-      return true
-    end
-    return [false, "A state has already been created with this abbreviation"]
-  end
-  
+  # def check_abbreviation_uniqueness
+  #   if self.new_document? && State.get(self.abbreviation.upcase).nil?
+  #     return true
+  #   end
+  #   return [false, "A state has already been created with this abbreviation"]
+  # end
 
 private
   def set_text_case
@@ -47,20 +48,13 @@ private
     self.name = mixed_case(name)
   end
 
+  def generate_identifier
+    #Pattern for Unique ID: class_key
+    self['identifier'] = self.class.to_s.downcase + '_' + abbreviation.downcase.gsub(/[^a-z0-9]/,'_').squeeze('_').gsub(/^\-|\-$/,'') if new?
+  end
+
   def mixed_case(name)
      name.downcase.gsub(/\b\w/) {|first| first.upcase }
-  end
-  
-#  =begin
-#    TODO mod create_or_update to update existing and trap insert failures
-#  =end
-  def self.create_or_update(options = {})
-    abbreviation = options.delete('abbreviation')
-    record = State.get(options['abbreviation']) || State.new
-    record.attributes = options
-    record.save!
-    
-    record
   end
   
 end
