7537796652d4a0dc5c4e46cdf737d905136bd44c
Dan Thomas <dan.thomas@ipublic.org>
2010-05-12 13:57:20 -0400

    moved :simple time definition to initializers/time_formats.rb

--- config/environment.rb
+++ config/environment.rb
@@ -52,6 +52,7 @@ Rails::Initializer.run do |config|
   # Skip frameworks you're not going to use. To use Rails without a database,
   # you must remove the Active Record framework.
   # config.frameworks -= [ :active_record, :active_resource, :action_mailer ]
+  # config.frameworks -= [ :active_record]
 
   # Activate observers that should always be running
   # config.active_record.observers = :cacher, :garbage_collector, :forum_observer
@@ -65,7 +66,3 @@ Rails::Initializer.run do |config|
   # config.i18n.default_locale = :de
   
 end
-
-ActiveSupport::CoreExtensions::Time::Conversions::DATE_FORMATS.merge!(
-  :simple => "%B %d, %Y"
-)
