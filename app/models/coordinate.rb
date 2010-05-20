f1a414bdc2a148f7a61866fd86f9d77e9665fdbe
Dan Thomas <dan.thomas@ipublic.org>
2010-05-20 18:21:16 -0400

    tinkering

--- app/models/coordinate.rb
+++ app/models/coordinate.rb
@@ -5,9 +5,8 @@ class Coordinate < Hash
   
   property :latitude
   property :longitude
-  property :coordinate_source_name
   
-  validates_presence_of [:latitude, :longitude]
-  validates_numericality_of [:latitude, :longitude]
+  # validates_presence_of [:latitude, :longitude]
+  # validates_numericality_of [:latitude, :longitude]
   
 end
