00db12ac90998b2c2872f67c44aca490d78bfe98
Dan Thomas <dan.thomas@ipublic.org>
2010-06-27 10:21:06 -0400

    added full and full_with_zone formats

--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -2,4 +2,6 @@
   Time::DATE_FORMATS[:month_and_year] = "%B %Y"
   Time::DATE_FORMATS[:short_ordinal] = lambda { |time| time.strftime("%B #{time.day.ordinalize}") }
   Time::DATE_FORMATS[:simple] = "%B %d, %Y"
+  Time::DATE_FORMATS[:full] = "%b %d, %Y %I:%M %p"
+  Time::DATE_FORMATS[:full_with_zone] = "%b %d, %Y %I:%M %p %Z"
   
