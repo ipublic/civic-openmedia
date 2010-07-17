a239632aa9f043770237da0ebd2b7f29114e6e25
Dan Thomas <dan.thomas@ipublic.org>
2010-07-17 17:15:49 -0400

    set email property as unique_id

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -3,6 +3,7 @@ class Contact < CouchRestRails::Document
   require 'address'
 
   use_database :site
+  unique_id :email
 
   property :first_name
   property :last_name
@@ -17,11 +18,12 @@ class Contact < CouchRestRails::Document
   timestamps!
 
   ## Validations
-  validates_presence_of :first_name, :last_name
+  # specifyied as unique_id, email is automatically included in validation 
   
   ## CouchDB Views
   # query with Organization.by_name
   view_by :last_name, :first_name
   view_by :first_name, :last_name
+  view_by :name, :email
 
 end
