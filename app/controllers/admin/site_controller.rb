d9ed4c75f6faa5068cdee5c622c127038e3d8e34
Dan Thomas <dan.thomas@ipublic.org>
2010-07-18 09:51:33 -0400

    corrected routing issues

--- app/controllers/admin/site_controller.rb
+++ app/controllers/admin/site_controller.rb
@@ -12,14 +12,13 @@ class Admin::SiteController < ApplicationController
   
   def create
     @site = Site.new(params[:site])
-    @gnis = Gnis.new(params[:gnis])
-    
-    @site.gnis = @gnis
+    @site.gnis = Gnis.new(params[:gnis])
+#    @gnis = Gnis.new(params[:gnis])
+#    @site.gnis = @gnis
 
     if @site.save
       flash[:notice] = 'Site successfully created.'
-      redirect_to([:admin, @site])
-#      redirect_to(admin_site_path(@site))
+      redirect_to(admin_site_path)
     else
       flash[:error] = 'Unable to create Site.'
       render :action => "new"
@@ -43,16 +42,29 @@ class Admin::SiteController < ApplicationController
 
   def update
     @site = Site.first
-    @gnis = Gnis.new(params[:gnis])
+    @updated_site = Site.new(params[:site])
+    
+    @revs = @site['_rev'] + ' ' + @updated_site['rev']
+    
+    if @site['_rev'].eql?(@updated_site.delete("rev"))
+      @updated_site.delete("couchrest-type")
+      @updated_site.gnis = Gnis.new(params[:gnis])
 
-    @site.gnis = @gnis
-
-    respond_to do |format|
-      if @site.update_attributes(params[:site])
-        flash[:notice] = 'Successfully updated site settings.'
-        format.html { redirect_to(@site) }
-        format.xml  { head :ok }
-      else
+      respond_to do |format|
+#        if @site.update_attributes(params[:site])
+        if @site.update_attributes(@updated_site)
+          flash[:notice] = 'Successfully updated site settings.'
+          format.html { redirect_to(admin_site_path) }
+          format.xml  { head :ok }
+        else
+          format.html { render :action => "edit" }
+          format.xml  { render :xml => @site.errors, :status => :unprocessable_entity }
+        end
+      end
+    else
+      # Document revision is out of sync
+      flash[:notice] = 'Update conflict. Site settings have been updated elsewhere, reload Site page, then update again. ' + @revs
+      respond_to do |format|
         format.html { render :action => "edit" }
         format.xml  { render :xml => @site.errors, :status => :unprocessable_entity }
       end
