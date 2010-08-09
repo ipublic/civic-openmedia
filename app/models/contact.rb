9b0e1e1e7b871a471c36eeb6b901ff67364fa488
Dan Thomas <dan.thomas@ipublic.org>
2010-08-09 11:55:55 -0400

    changed class from CouchRestRails to Hash

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,29 +1,35 @@
-class Contact < CouchRestRails::Document
+class Contact < Hash
+#class Contact < CouchRestRails::Document
 
   require 'address'
+  require 'phone'
 
-  use_database :site
-  unique_id :email
+  include ::CouchRest::CastedModel
+  include Validatable
+
+#  use_database :site
+#  unique_id :email
 
   property :first_name
   property :last_name
   property :job_title
-  property :phone
   property :email
   property :website_url
-  property :address, :cast_as => 'Address'
-#  property :addresses, :cast_as => ['Address'] # support multiple addresses for contact
+  property :phones, :cast_as => ['Phone']
+  property :addresses, :cast_as => ['Address']
   property :note
 
-  timestamps!
+  #  property :organization_id
+  # timestamps!
 
   ## Validations
   # specifyied as unique_id, email is automatically included in validation 
   
   ## CouchDB Views
-  # query with Organization.by_name
-  view_by :last_name, :first_name
-  view_by :first_name, :last_name
-  view_by :name, :email
+  # query with Contact.by_last_name_and_first_name
+  # view_by :last_name, :first_name
+  # view_by :first_name, :last_name
+  # view_by :organization_name
+  # view_by :name, :email
 
 end
