212580af490d82a7a0e320d70ff05c832988992b
Dan Thomas <dan.thomas@ipublic.org>
2010-08-15 12:13:14 -0400

    initial commit

---
+++ app/helpers/organizations_helper.rb
@@ -0,0 +1,11 @@
+module OrganizationsHelper
+  
+  def concat_organization_name_abbreviation(org)
+    unless org.abbreviation.blank?
+      result = org.name + ' (' + org.abbreviation + ')'
+    else
+      result = org.name
+    end
+  end
+
+end
