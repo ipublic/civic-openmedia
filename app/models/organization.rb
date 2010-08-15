b213dc036257b13ef5a46e3abddc1a886b782d53
Dan Thomas <dan.thomas@ipublic.org>
2010-08-15 17:10:54 -0400

    added identifier property and callback method

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -6,8 +6,10 @@ class Organization < CouchRestRails::Document
   require 'contact'
   
   use_database :site
+  unique_id :identifier 
 
   property :name
+  property :identifier
   property :abbreviation
   property :email
   property :website_url
@@ -22,10 +24,14 @@ class Organization < CouchRestRails::Document
   ## Validations
   validates_presence_of :name
   
+  ## Callbacks
+  before_save :generate_identifier
+  
   ## CouchDB Views
   # query with Organization.by_name
   view_by :name
   view_by :abbreviation
+  view_by :name, :identifier
   
   def get_creator_content_documents
     list = ContentDocument.by_creator_organization_id(:key => self['identifier']) # unless new?
@@ -34,9 +40,9 @@ class Organization < CouchRestRails::Document
 private
   def generate_identifier
     unless abbreviation.blank? 
-      self['identifier'] = self.class.to_s.pluralize.downcase + '_'+ abbreviation.rstrip.downcase.gsub(/[^a-z0-9]/,'_').squeeze('_').gsub(/^\-|\-$/,'') if new?
+      self['identifier'] = abbreviation.rstrip.downcase.gsub(/[^a-z0-9]/,'_').squeeze('_').gsub(/^\-|\-$/,'') if new?
     else
-      self['identifier'] = self.class.to_s.pluralize.downcase + '_'+ name.rstrip.downcase.gsub(/[^a-z0-9]/,'_').squeeze('_').gsub(/^\-|\-$/,'') if new?
+      self['identifier'] = name.rstrip.downcase.gsub(/[^a-z0-9]/,'_').squeeze('_').gsub(/^\-|\-$/,'') if new?
     end
 #    self['identifier'] = name.downcase.gsub(/[^a-z0-9]/,'_').squeeze('_').gsub(/^\-|\-$/,'') if new?
   end
