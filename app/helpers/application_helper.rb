3772d470ebf343571877746d56972ee8a5cd271d
Dan Thomas <dan.thomas@ipublic.org>
2010-08-22 15:33:39 -0400

    added form_dates_to_json method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,13 @@ module ApplicationHelper
   def boolean_to_human(test)
     test  ? "Yes" : "No"
   end
+  
+  def form_dates_to_json(year, month, day)
+    # return if new_attributes.nil?
+    # attributes = new_attributes.dup
+    # args.each do |k, v| k.include?("(") ?  
+
+    Time.local(year.to_i, month.to_i, day.to_i)
+  end
+  
 end
